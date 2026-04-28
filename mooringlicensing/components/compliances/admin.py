2a8edf3874ddd1867efac8fad74c82722afa7de4
Ashley Woodiss-Field <ash.woodissfield@dbca.wa.gov.au>
2026-04-28 09:41:32 +0800

    Added: Compliance Admin

--- mooringlicensing/components/compliances/admin.py
+++ mooringlicensing/components/compliances/admin.py
@@ -7,3 +7,8 @@ from mooringlicensing.components.compliances import models
 class ComplianceAmendmentReasonAdmin(admin.ModelAdmin):
     list_display = ['reason']
 
+@admin.register(models.Compliance)
+class ComplianceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'lodgement_number', 'proposal__lodgement_number', 'processing_status', 'approval__lodgement_number',]
+    readonly_fields = ['proposal','approval','requirement']
+    search_fields = ['id', 'lodgement_number', 'proposal__lodgement_number' ,'approval__lodgement_number',]
